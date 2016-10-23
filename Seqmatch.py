6f008ebfea52a5a372d79c19db3d9990d199f451
Drew <kalaborative94@gmail.com>
2016-10-22 23:43:34 -0500

    New function update

--- Seqmatch.py
+++ Seqmatch.py
@@ -2,12 +2,26 @@ from difflib import SequenceMatcher
 
 
 def similarity(sing, itr):
-	queue = {}
-	a, b, c = itr
-	compareA = SequenceMatcher(None, sing, a).ratio()
-	compareB = SequenceMatcher(None, sing, b).ratio()
-	compareC = SequenceMatcher(None, sing, c).ratio()
-	queue[a] = compareA
-	queue[b] = compareB
-	queue[c] = compareC
-	return max(queue, key=queue.get)
+    queue = {}
+    a, b, c = itr
+    compareA = SequenceMatcher(None, sing, a).ratio()
+    compareB = SequenceMatcher(None, sing, b).ratio()
+    compareC = SequenceMatcher(None, sing, c).ratio()
+    queue[a] = compareA
+    queue[b] = compareB
+    queue[c] = compareC
+    return max(queue, key=queue.get)
+
+
+def apostrophe_checker(inpList):
+    for q in inpList:
+        if "'" in q:
+            foundApos = inpList.index(q)
+            AposSliced = slice(inpList.index(q), (inpList.index(q) + 2))
+            tText = inpList[AposSliced]
+            AposWord = "".join(inpList[AposSliced])
+            newList = [q for q in inpList if q not in tText]
+            newList.insert(foundApos, AposWord)
+            return newList
+        else:
+            return inpList
