468d65a1edd56507f0d3673d86b8a3e4eb9e1db5
neau <neau@users.noreply.github.com>
2019-07-21 02:55:46 +0300

    13 points a run :^)

--- autoskip.py
+++ autoskip.py
@@ -8,10 +8,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 print("Here we go!")
-print('Enter email: ')
-email = input()
-print('Enter pass: ')
-passw = input()
+#print('Enter email: ')
+email = ''
+#print('Enter pass: ')
+passw = ''
 
 
 global driver
@@ -26,7 +26,11 @@ chrome_options.add_experimental_option('prefs', {
 chrome_options.add_argument('disable-infobars')
 driver = driver.Chrome(options=chrome_options)
 driver.implicitly_wait(5)
-#driver.maximize_window()
+driver.maximize_window()
+skipped = 0
+d = {'وا':'waa','رَ':'ra','دَ':'da','وي':'wii','دا':'daa','زود':'zuud','زار':'zaar','داد':'daad','زَ':'za','وَ':'w','زو':'zuu','راي':'raay','يَ':'ya','زي':'zii','زو':'zuu','دي':'dii','زا':'zaa','رو':'ruu','رود':'ruud','رَ':'ra','را':'raa','دور':'duur','ي':'ii','دو':'duu','دود':'duud','و':'uu','راو':'raaw','ا':'a'}
+
+#Begin 
 driver.get("https://www.duolingo.com/log-in")
 driver.find_element_by_xpath('/html/body/div[5]/div/div/form/div[1]/label[1]/div/input').send_keys(email)
 driver.find_element_by_xpath('/html/body/div[5]/div/div/form/div[1]/label[2]/div/input').send_keys(passw)
@@ -37,10 +41,10 @@ def x(xpath):
     return driver.find_element_by_xpath(xpath)
 
 def skip():
-    while True:
-        driver.find_element_by_xpath('//*[@id="root"]/div/div/div/div/div[3]/div/div/div[1]/button').click()
-        x('//*[@id="root"]/div/div/div/div/div[3]/div/div/div[4]/button').click()
-        sleep(1)
+    print('Skipping...')
+    x('//*[@id="root"]/div/div/div/div/div[3]/div/div/div[1]/button').click()
+    x('//*[@id="root"]/div/div/div/div/div[3]/div/div/div[4]/button').click()
+    sleep(0.5)
 
 def url():
     driver.get('https://www.duolingo.com/skill/ar/Alphabet1/practice')
@@ -48,20 +52,51 @@ def url():
 
 def solve():
     challenge = x('//*[@id="root"]/div/div/div/div/div[2]/div/div/div/h1/span').text    
-    print(challenge)
+    print('\nNew challenge: ',challenge)
     try:
-        if challenge:
-            x('//*[@id="root"]/div/div/div/div/div[3]/div/div/div[1]/button').click()
+        if 'Match the pairs' in challenge:
+            print('TYPE: Math the pairs')
+            skip()
+        elif 'What sound does this make?' in challenge:
+            print('TYPE: What sound does this make?')
+            challengeLetter = x('//*[@id="root"]/div/div/div/div/div[2]/div/div/div/div/div[1]/div/div[1]/span').text
+            print('Challenge is: ', challengeLetter)
+            c = d.get(challengeLetter)
+            print('Answer should be: ', c)
+
+            a1 = x('//*[@id="root"]/div/div/div/div/div[2]/div/div/div/div/div[2]/div[1]/label/div[2]').text
+            print('Answer1: ', a1)
+            a2 = x('//*[@id="root"]/div/div/div/div/div[2]/div/div/div/div/div[2]/div[2]/label/div[2]').text
+            print('Answer2: ', a2)
+            a3 = x('//*[@id="root"]/div/div/div/div/div[2]/div/div/div/div/div[2]/div[3]/label/div[2]').text
+            print('Answer3: ', a3)
+            
+            if c == a1:
+                print('>>1')
+                x('//*[@id="root"]/div/div/div/div/div[2]/div/div/div/div/div[2]/div[1]/label/div[2]').click()
+            elif c == a2:
+                print('>>2')
+                x('//*[@id="root"]/div/div/div/div/div[2]/div/div/div/div/div[2]/div[2]/label/div[2]').click()
+            elif c == a3:
+                print('>>3')
+                x('//*[@id="root"]/div/div/div/div/div[2]/div/div/div/div/div[2]/div[3]/label/div[2]').click()
+            else:
+                print('Challenge not found, letter: ', challengeLetter, ', Options', b1.text, b2.text, b3.text)
+                skip()
+            # Assuming correct answer is selected, check and move on
+            # Check
+            x('//*[@id="root"]/div/div/div/div/div[3]/div/div/div[3]/button').click()
+            # Continue
             x('//*[@id="root"]/div/div/div/div/div[3]/div/div/div[4]/button').click()
-            sleep(0.5)
-            challenge = ''
+            sleep(0.5)               
         else:
-            print('Challenge is empty, getting text')
+            print('ENTERED ELSE, SKIPPING for'       )
+            skip()
     except:
-        print('Entered exception, trying again')
-        pass
-        url()
+        print('\nNo more challenges detected, refreshing', challenge)
+        return
     solve()
 
-url()
-solve()
+while True:
+    url()
+    solve()
